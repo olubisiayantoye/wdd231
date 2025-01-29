8d3cabea35f310a64fd17d13d2f92f55f13c7202
olubisiayantoye <bisikingson@gmail.com>
2025-01-29 15:03:33 +0100

    new file:   chamber/data/members.json 	modified:   chamber/directory.html 	new file:   chamber/images/cafe-logo.webp 	new file:   chamber/images/crea8.webp 	new file:   chamber/images/fairdeal.webp 	new file:   chamber/images/green-planet-logo.webp 	new file:   chamber/images/gym-zone-logo.webp 	new file:   chamber/images/humane.webp 	new file:   chamber/images/instagram-logo.svg 	new file:   chamber/images/kala.webp 	new file:   chamber/images/meralco.webp 	new file:   chamber/images/pest-control.webp 	new file:   chamber/images/sari-sari.webp 	new file:   chamber/images/smart-start-logo.webp 	new file:   chamber/images/techhive-logo.webp 	new file:   chamber/images/trust-large.webp 	new file:   chamber/images/twitter-icon.webp 	new file:   chamber/images/twitter-logo.svg 	new file:   chamber/images/youtube-icon.webp 	new file:   chamber/images/youtube-with-circle.svg 	modified:   chamber/scripts/script.js 	modified:   chamber/styles/styles.css

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -29,3 +29,29 @@ function showList() {
 	display.classList.remove("grid");
 }
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('data/members.json')
+      .then(response => response.json())
+      .then(data => {
+          const container = document.getElementById('members-container');
+
+          data.forEach(member => {
+              const section = document.createElement('section');
+
+              section.innerHTML = `
+                  <img src="${member.image_icon}" alt="${member.name}" />
+                  <h3>${member.name}</h3>
+                  <p>${member.address}</p>
+                  <p><strong>Phone:</strong> ${member.phone_number}</p>
+                  <a href="${member.website_url}" target="_blank">Visit Website</a>
+                  <p><strong>Membership Level:</strong> ${member.membership_level}</p>
+              `;
+
+              container.appendChild(section);
+          });
+      })
+      .catch(error => console.error('Error loading member.json:', error));
+});
