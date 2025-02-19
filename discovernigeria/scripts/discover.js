b701a154d42fa0a87d61734f57f6484f5cb82027
olubisiayantoye <bisikingson@gmail.com>
2025-02-19 12:05:59 +0100

    modified:   discovernigeria/attractions.html 	new file:   discovernigeria/contact.html 	new file:   discovernigeria/cuisine.html 	modified:   discovernigeria/culture.html 	new file:   discovernigeria/images/akara.jpg 	new file:   discovernigeria/images/attire.jpg 	new file:   discovernigeria/images/dance.jpg 	new file:   discovernigeria/images/egusi.jpg 	new file:   discovernigeria/images/festival.jpg 	new file:   discovernigeria/images/jollof-rice.jpg 	new file:   discovernigeria/images/moimoi.jpg 	new file:   discovernigeria/images/music.jpg 	new file:   discovernigeria/images/nigeria-coat-arms-25633847.webp 	new file:   discovernigeria/images/suya.jpg 	modified:   discovernigeria/index.html 	modified:   discovernigeria/scripts/discover.js 	modified:   discovernigeria/styles/styles.css

--- discovernigeria/scripts/discover.js
+++ discovernigeria/scripts/discover.js
@@ -13,3 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
       mainMenu.classList.toggle('show'); // Add or remove the "show" class
     });
   });
+
+
+// Lazy Loading Images
+document.addEventListener("DOMContentLoaded", () => {
+  const lazyImages = document.querySelectorAll("img[data-src]");
+
+  const loadImage = (image) => {
+      image.setAttribute("src", image.getAttribute("data-src"));
+      image.onload = () => {
+          image.removeAttribute("data-src");
+      };
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              loadImage(entry.target);
+              observer.unobserve(entry.target);
+          }
+      });
+  });
+
+  lazyImages.forEach(image => {
+      observer.observe(image);
+  });
+});  
